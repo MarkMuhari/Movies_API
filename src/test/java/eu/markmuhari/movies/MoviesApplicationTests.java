b05456c1aa13b49c35c69e24a882e88587185591
MarkMuhari <markmuhari.eu@gmail.com>
2023-03-04 18:05:52 +0100

    Basic starter files

---
+++ src/test/java/eu/markmuhari/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.markmuhari.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
